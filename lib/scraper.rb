4b5d5285a3d5c49276a861675d7f5edb237c50bc
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-29 14:35:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,7 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    students = {}
 
-
-    
-
-  end
 
  def self.scrape_profile_page(profile_url)
     student_profile = {}
