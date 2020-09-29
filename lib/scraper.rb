ee8e5d0f0b0a553dc26adbda8fb84cf8681a5067
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-29 13:27:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-    links
+    links = profile_page.css("")
 
   end
 
